35df9d61dd6d517bc5fd8efa60c4283c6382276d
Ibukunola <bulun1001@gmail.com>
2025-01-06 20:41:58 +0100

    feat: add some basic javascript commands

---
+++ 01_basics/app.js
@@ -0,0 +1,166 @@
+// Console Messages
+console.warn("This is a warning")
+
+console.clear()
+
+console.table({name: "Mareola", age: 25})
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Variables
+// Declaring Variables
+let name;
+
+
+// Assigning Values
+name = "banana";
+
+let algo = name+"rest";
+console.log(algo);
+
+let ASD = "fjjfj";
+console.log(ASD);
+
+let myName = "Ibukunola"
+let whatToBecomeInLife = "Software Engineer"
+let gender = "Male"
+let twitterHandle = "Mareola_Official"
+
+console.log(myName, whatToBecomeInLife, gender, twitterHandle)
+
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Primitive Types
+// 1. Number
+let num = true
+console.log(typeof num)
+
+// Basic Math
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2  * 2)
+console.log(2  / 2)
+console.log(2  % 2)
+console.log(2  ** 2)
+
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Increment and Decrement Operators
+let counter = 0
+counter++
+counter++
+counter--
+console.log(counter)
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Challenge
+let firstFavNum = 10
+let secondFavNum = 15
+
+console.log(firstFavNum + secondFavNum)
+console.log(firstFavNum - secondFavNum)
+console.log(firstFavNum * secondFavNum)
+console.log(firstFavNum / secondFavNum)
+console.log(firstFavNum % secondFavNum)
+console.log(firstFavNum ** secondFavNum)
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Boolean
+
+let isTrue = NaN
+let isUndefined = undefined
+let isNull = null
+
+console.log(isTrue)
+console.log(isUndefined)
+console.log(isNull)
+
+// Challenge
+let isJsProgrammingLanguage = true
+let isJsHard = false
+let favNumb = 10
+let secondFavNumb
+
+console.log(favNumb + secondFavNumb)
+
+console.log(isTrue)
+console.log(isUndefined)
+console.log(isNull)
+
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Comparison and Equality Operators
+let cl = console.log
+cl(10 === "10")
+cl(10 == "10")
+cl(10 > "10")
+cl(10 < "10")
+cl(10 == 10)
+cl(10 <= 10)
+cl(10 > 10)
+cl(10 !== "10")
+
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Challenge
+let firstFavNumb = 10
+let secondFavNumbs = 15
+cl(firstFavNumb > secondFavNumbs)
+cl(firstFavNumb < secondFavNumbs)
+cl(firstFavNumb >= secondFavNumbs)
+cl(firstFavNumb <= secondFavNumbs)
+cl(firstFavNumb === secondFavNumbs)
+cl(firstFavNumb == secondFavNumbs)
+cl(firstFavNumb !== secondFavNumbs)
+cl(firstFavNumb != secondFavNumbs)
+
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Strings
+let lastName = `Mabs`
+let firstName = `Ibukunola`
+let fullName = `${firstName} ${lastName}`
+cl(fullName)
+
+// Contatenation
+let mobile = `iphone`
+let version  = `16`
+cl(mobile.concat(version))
+
+// Append
+let numb = `5`
+cl(numb+=1)
+cl(numb.length)
+
+// Cases
+let aresnal = "HuxnHud"
+cl(aresnal.toLowerCase())
+cl(aresnal.toUpperCase())
+
+cl("")
+cl("")
+cl("")
+
+// Slice Method
+
